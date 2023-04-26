4f25c318f9c0d636915688573e3a5b91dfb3f8f0
nurbol0tt <fairtail764@gmail.com>
2023-04-27 00:29:20 +0600

    removed unnecessary fields

--- store/urls.py
+++ store/urls.py
@@ -33,9 +33,9 @@ urlpatterns = [
     path('api/', include('user.urls')),
     path('api/', include('basket.urls')),
     path('api/', include('payment.urls')),
-    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    # path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
