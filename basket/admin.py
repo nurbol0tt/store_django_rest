cbac85c118219946bfd880244bbffccc9f2a56dc
nurbol0tt <fairtail764@gmail.com>
2023-04-27 00:33:43 +0600

    improved readability in basket app tables

--- basket/admin.py
+++ basket/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 
-from basket.models import Basket, CartProduct, Cart, PurchasesHistory
+from basket.models import CartProduct, Cart, PurchasesHistory
 
 # Register your models here.
-admin.site.register(Basket)
 admin.site.register(Cart)
 admin.site.register(CartProduct)
 admin.site.register(PurchasesHistory)
